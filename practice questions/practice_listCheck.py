4d2f87eb1e6c75bab388976d29c3f0808dfe0aad
Jake-sleepypillows <jacobdylanfleming@gmail.com>
2021-03-30 17:11:38 +1100

    Questions added to each file

--- practice questions/practice_listCheck.py
+++ practice questions/practice_listCheck.py
@@ -1,4 +1,13 @@
- # Print numbers in list less than number.
+"""
+Q) Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5.
+
+Extras:
+1. Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+2. Write this in one line of Python.
+"""
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 3, 5, 1, 8, 5, 4, 9, 11, 50, 23, 645, 34, 12, 11, 10, 2, 3, 4]
 list_check = 9
 index = 0
--- practice questions/practice_oddOrEven.py
+++ practice questions/practice_oddOrEven.py
@@ -1,4 +1,12 @@
-# Odd or Even
+"""
+Q) Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+1. If the number is a multiple of 4, print out a different message.
+2. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+"""
+
+
 num1 = int(input("Pick a number: "))
 diviser = int(input("Pick a second number: "))
 if num1 % diviser == 0:
